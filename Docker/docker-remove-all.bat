33c24bc5b83738385047059194f8f596a908c2eb
crperez.informatica <crperez.informatica@gmail.com>
2019-02-16 10:34:38 +0000

    Deleted Docker

--- Docker/docker-remove-all.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-FOR /f "tokens=*" %%i IN ('docker ps -q') DO docker stop %%i
-FOR /f "tokens=*" %%i IN ('docker ps -aq') DO docker rm %%i
-FOR /f "tokens=*" %%i IN ('docker images --format "{{.ID}}"') DO docker rmi %%i
-cmd /k "docker volume ls -qf dangling=true"
