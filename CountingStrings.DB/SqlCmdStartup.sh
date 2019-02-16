7b6cc888fbeb4053d42cc815279244d24120a8c5
Cristian Perez <crperez.informatica@gmail.com>
2019-02-16 23:31:15 +0100

    echo test

--- CountingStrings.DB/SqlCmdStartup.sh
+++ CountingStrings.DB/SqlCmdStartup.sh
@@ -1,1 +1,1 @@
-sleep 120 && /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i SqlCmdScript.sql
+sleep 120 && echo $SA_PASSWORD && echo ${SA_PASSWORD} && /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P ${SA_PASSWORD} -d master -i SqlCmdScript.sql
